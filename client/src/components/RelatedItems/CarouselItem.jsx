b5cfa69e9ab294d4ccc825b1650c8541341bde4c
Trevor Leung <humormy@gmail.com>
2022-02-14 11:56:45 -0800

    added parts of the card

--- client/src/components/RelatedItems/CarouselItem.jsx
+++ client/src/components/RelatedItems/CarouselItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 class CarouselItem extends React.Component {
   constructor(props) {
@@ -10,9 +11,41 @@ class CarouselItem extends React.Component {
 
   render() {
     return (
-      <div> </div>
+      <Wrapper>
+        <CarouselSlot>
+          Image
+        </CarouselSlot>
+        <button type="button">
+          add
+        </button>
+        <div>
+          Name
+        </div>
+        <div>
+          Category
+        </div>
+        <div>
+          Cost
+        </div>
+        <div>
+          Stars
+        </div>
+      </Wrapper>
     );
   }
 }
+
+export const Wrapper = styled.div`
+  display: grid;
+  margin: 15px;
+`;
+
+export const CarouselSlot = styled.div`
+  background: green;
+  text-align: center;
+  color: white;
+  height: 150px;
+  width: 150px;
+`;
 
 export default CarouselItem;
--- client/src/components/RelatedItems/MultiDisplayCarousel.jsx
+++ client/src/components/RelatedItems/MultiDisplayCarousel.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import styled from 'styled-components';
+import CarouselItem from './CarouselItem';
 
 class MultiDisplayCarousel extends React.Component {
   constructor(props) {
@@ -38,16 +39,10 @@ class MultiDisplayCarousel extends React.Component {
           direction={this.state.direction}
           position={this.state.position}
         >
-          <CarouselSlot>Item 1</CarouselSlot>
-          <CarouselSlot>Item 2</CarouselSlot>
-          <CarouselSlot>Item 3</CarouselSlot>
-          <CarouselSlot>Item 4</CarouselSlot>
-          <CarouselSlot>Item 5</CarouselSlot>
-          <CarouselSlot>Item 6</CarouselSlot>
-          <CarouselSlot>Item 7</CarouselSlot>
-          <CarouselSlot>Item 8</CarouselSlot>
-          <CarouselSlot>Item 9</CarouselSlot>
-          <CarouselSlot>Item 10</CarouselSlot>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
         </CarouselContainer>
         <button type="button" onClick={() => this.doSlide('left', this.state.position - 1)}>Prev</button>
         <button type="button" onClick={() => this.doSlide('right', this.state.position + 1)}>Next</button>
