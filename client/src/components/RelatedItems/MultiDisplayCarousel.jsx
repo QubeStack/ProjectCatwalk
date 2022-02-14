97e8c0cb25ad2537c5b33a4c016b3bd9a6532b6c
Trevor Leung <humormy@gmail.com>
2022-02-14 10:26:59 -0800

    made some changes due to linter

--- client/src/components/RelatedItems/MultiDisplayCarousel.jsx
+++ client/src/components/RelatedItems/MultiDisplayCarousel.jsx
@@ -7,32 +7,36 @@ class MultiDisplayCarousel extends React.Component {
     this.state = {
       position: 0,
       direction: 'right',
-      slide: false
+      slide: false,
     };
   }
 
-  doSlide = (direction, position) => {
+  doSlide(direction, position) {
+    let newPosition = position;
     if (position < 0) {
-      position = 0;
+      newPosition = 0;
     }
     this.setState({
       slide: true,
-      direction
+      direction,
     });
 
     setTimeout(() => {
       this.setState({
         direction: undefined,
-        position
+        position: newPosition,
       });
     }, 100);
   }
 
-
   render() {
     return (
-      <>
-        <CarouselContainer slide={this.state.slide} direction={this.state.direction} position={this.state.position}>
+      <HideOverflowContainer>
+        <CarouselContainer
+          slide={this.state.slide}
+          direction={this.state.direction}
+          position={this.state.position}
+        >
           <CarouselSlot>Item 1</CarouselSlot>
           <CarouselSlot>Item 2</CarouselSlot>
           <CarouselSlot>Item 3</CarouselSlot>
@@ -44,12 +48,16 @@ class MultiDisplayCarousel extends React.Component {
           <CarouselSlot>Item 9</CarouselSlot>
           <CarouselSlot>Item 10</CarouselSlot>
         </CarouselContainer>
-        <button onClick={() => this.doSlide('left', this.state.position - 1)}>Prev</button>
+        <button type='foo' onClick={() => this.doSlide('left', this.state.position - 1)}>Prev</button>
         <button onClick={() => this.doSlide('right', this.state.position + 1)}>Next</button>
-      </>
+      </HideOverflowContainer>
     );
   }
 }
+
+const HideOverflowContainer = styled.div`
+  overflow: hidden;
+`;
 
 const CarouselContainer = styled.div`
   display: flex;
@@ -57,14 +65,14 @@ const CarouselContainer = styled.div`
   transition: 'transform 0.2s ease';
   transform: ${(props) => {
     if (props.direction === 'right') {
-      return "translateX(-" + (13.5 * props.position + 1) + "%)";
+      return `translateX(-${13.5 * props.position + 1}%)`;
     }
     if (props.direction === 'left') {
-      return "translateX(-" + (13.5 * props.position - 1) + "%)";
+      return `translateX(-${13.5 * props.position - 1}%)`;
     }
-    return "translateX(-" + (13.5 * props.position) + "%)";
-    }
-  }};
+    return `translateX(-${13.5 * props.position}%)`;
+  }
+}};
 `;
 
 export const CarouselSlot = styled.div`
