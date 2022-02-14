bfd755af895f5e5490248e4095d8550ba743da85
Joe Wagstaff <joe.wagstaff@gmail.com>
2022-02-14 10:16:44 -0800

    fixed linter rule and started add button

--- client/src/components/QandA/AskQuestion.jsx
+++ client/src/components/QandA/AskQuestion.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const AskAQuestion = styled.div`
+const AskAQuestion = styled.button`
   border-style: solid;
   border-color: green;
   padding: 10px;
   width: 200px;
+  display: inline-block;
 `;
 
 const AskQuestion = (props) => (
--- client/src/components/QandA/QuestionsView.jsx
+++ client/src/components/QandA/QuestionsView.jsx
@@ -2,26 +2,51 @@ import React from 'react';
 import styled from 'styled-components';
 import QAListEntry from './QAListEntry';
 
-const MoreQuestions = styled.div`
+const MoreQuestions = styled.button`
   border-style: solid;
   border-color: green;
   padding: 10px;
   width: 200px;
+  grid-column-start: 1;
 `;
-function QuestionsView(props) {
-  const { questions } = props;
 
-  return (
-    <div>
+class QuestionsView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 2,
+    };
+    this.showMoreQuestions = this.showMoreQuestions.bind(this);
+  }
+
+  showMoreQuestions() {
+    const { count } = this.state;
+    this.setState({
+      count: count + 2,
+    });
+  }
+
+  render() {
+    const { count } = this.state;
+    const { questions } = this.props;
+    return (
       <div>
-        {questions.map((question) =>
-          <QAListEntry key={question.question_id} id={question.question_id} question={question.question_body}/>)}
+        <div>
+          {questions.slice(0, count).map((question) => (
+            <QAListEntry
+              key={question.question_id}
+              id={question.question_id}
+              question={question.question_body}
+            />
+          ))}
+        </div>
+        <MoreQuestions onClick={this.showMoreQuestions}>
+          More Answered Questions
+        </MoreQuestions>
       </div>
-      <MoreQuestions>
-        More Answered Questions
-      </MoreQuestions>
-    </div>
-  );
+    );
+  }
 }
 
 export default QuestionsView;
