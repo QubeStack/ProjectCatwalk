7ff3e625c4454b139c53da035c324c72f61eea66
Waydizzle <marblewaylon@gmail.com>
2022-02-14 16:48:34 -0700

    delete API key

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
-const key = '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';
+const key = 'YOUR KEY HERE';
 
 app.use(express.static('client/dist'));
 app.use(express.json());
