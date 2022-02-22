e1895e9624dc46a246a859b1bd5948e6fbba42f9
Trevor Leung <humormy@gmail.com>
2022-02-21 17:54:10 -0800

    edited a test

--- tests/RelatedItems.test.js
+++ tests/RelatedItems.test.js
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable function-paren-newline */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-undef */
